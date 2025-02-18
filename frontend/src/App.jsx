4c916948188faec147dd44a79026f09877dab070
shahbazahmad1 <shahbaz.ahmad141@gmail.com>
2025-02-18 18:57:19 +0530

    Project Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
-import ProfilePage from "./pages/ProfilePAge";
+import ProfilePage from "./pages/ProfilePage";
 import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
